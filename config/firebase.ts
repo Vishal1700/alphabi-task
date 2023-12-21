c6f560c5f399c9b5b3246eb3dd95e3dabc1d3293
Nishad Vishal <88231328+Vishal1700@users.noreply.github.com>
2023-12-21 14:08:42 +0530

    Update firebase.ts

--- config/firebase.ts
+++ config/firebase.ts
@@ -4,11 +4,10 @@ import { getAuth } from 'firebase/auth'
 const firebaseConfig = {
   apiKey: "AIzaSyAXFTWxXXnlp3nnUvNq7vQr1EcCyLncX-8",
   authDomain: "alphabi-assignment-1403f.firebaseapp.com",
-  projectId: "alphabi-assignment-1403ft",
-  storageBucket: "alphabi-assignment.appspot.com",
+  projectId: "alphabi-assignment-1403f",
+  storageBucket: "alphabi-assignment-1403f.appspot.com",
   messagingSenderId: "555796458853",
   appId: "1:555796458853:web:b2cf102ee26a567929458c",
-  measurementId: "G-L1YTYPC49Y"
 };
 
 const app = initializeApp(firebaseConfig)
